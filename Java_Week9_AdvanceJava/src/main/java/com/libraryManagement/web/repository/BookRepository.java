78b87b577d427823f4c7841705ff8c1071733984
Abhishek7525 <65594267+Abhishek7525@users.noreply.github.com>
2022-04-20 15:07:15 +0530

    Delete Java_Week9_AdvanceJava directory

--- Java_Week9_AdvanceJava/src/main/java/com/libraryManagement/web/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.libraryManagement.web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.libraryManagement.web.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-	
-}
--- Java_Week9_AdvanceJava/src/main/java/com/libraryManagement/web/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.libraryManagement.web.service;
-
-public interface UserService {
-	
-	boolean validateUser(String username, String password);
-}
